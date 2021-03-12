7d629dd2ba7f48adc6cbced2128595615c58c77c
Vinicius Aguiar <vinicius.aguiar@dtidigital.com.br>
2021-03-12 15:55:18 -0300

    created location.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,11 +13,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocationFromGeolocator();
           },
           child: Text('Get Location'),
         ),
       ),
     );
+  }
+
+  void getLocationFromGeolocator() async {
+    Location location = Location();
+    await location.getCurrentLocation();
   }
 }
