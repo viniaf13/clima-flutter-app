9630ce7a4c15ea017c9f99502dc0a3a1e716b645
Vinicius Aguiar <vinicius.aguiar@dtidigital.com.br>
2021-03-15 10:31:05 -0300

    getting data from api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,11 +17,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             getLocationFromGeolocator();
+            getData();
           },
           child: Text('Get Location'),
         ),
       ),
     );
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    var data = response.body;
+    var dataDecoded = jsonDecode(data);
   }
 
   void getLocationFromGeolocator() async {
